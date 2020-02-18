4bfa5362e20562cb6172675323556e004dfc6fdb
pritamCodevian <pritam.bhalerao@codevian.com>
2020-02-18 19:26:20 +0530

    super admin can create an enterprise

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,179 +1,237 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import * as myGlobals from '../globalPath';
-import { IProfile } from '../profile/model/user-profile';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import * as myGlobals from "../globalPath";
+import { IProfile } from "../profile/model/user-profile";
 
-import { IUser } from '../models/user';
-
+import { IUser } from "../models/user";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
   public baseurl: any;
-  public user:any;
-  public IUser:IUser;
-  public employerProfile:IProfile;
- 
-  constructor(private http: HttpClient) { 
+  public user: any;
+  public IUser: IUser;
+  public employerProfile: IProfile;
+
+  constructor(private http: HttpClient) {
     this.baseurl = myGlobals.baseUrl;
-  
-  }
-  
-
-  getmeetup(){
-    return this.http.get<any>(this.baseurl+'api/getMeetupEvent').pipe(map((res:any)=>{    
-      return res;
-    }))
-  }
-  getTopRecruiter(){
-    return this.http.get<any>(this.baseurl+'api/getTopRecruiter').pipe(map((res:any)=>{    
-      return res;
-    }))
-  }
- subscribeNewsLetter(email){
-  
-    return this.http.get<any>(this.baseurl+'api/subcribe/'+email.email).pipe(map((res:any)=>{    
-      return res;
-    }))
-  }
-  getAllEmailCsv(){
-  
-    return this.http.get<any>(this.baseurl+'api/getAllEmailCsv').pipe(map((res:any)=>{    
-  
-      return res;
-
-    }))
   }
 
-
-register(info:any){
-  return this.http.post<any>(this.baseurl+'api/register',info).pipe(map((res:any)=>{
-    return res;
-  }));
-}
-
-emailVerification(info:any){
-  return this.http.put<any>(this.baseurl+'api/emailVerification',info).pipe(map((res:any)=>{
-    return res;
-  }));
-}
-
-getUserData(){
-  this.user= JSON.parse(localStorage.getItem("currentUser"));
-  if(this.user){
-    return this.user.userInfo;
-  }else{
-    return "no";
+  getmeetup() {
+    return this.http.get<any>(this.baseurl + "api/getMeetupEvent").pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
   }
-  
-}
-
-getUser(){
-  this.user= JSON.parse(localStorage.getItem("currentUser"));
-  if(this.user){
-    return this.user;
-  }else{
-    return "no";
+  getTopRecruiter() {
+    return this.http.get<any>(this.baseurl + "api/getTopRecruiter").pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
   }
-  
-}
-
-getIUserData():IUser{
-  this.user= JSON.parse(localStorage.getItem("currentUser"));
-  if(this.user){
-    return this.user.userInfo;
-  }else{
-    return null;
+  subscribeNewsLetter(email) {
+    return this.http
+      .get<any>(this.baseurl + "api/subcribe/" + email.email)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
   }
-  
-}
+  getAllEmailCsv() {
+    return this.http.get<any>(this.baseurl + "api/getAllEmailCsv").pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
 
-getUserProfile(userRole){
-  return this.http.get<IProfile>(this.baseurl+'api/getUserProfile/'+userRole).pipe(map((res:IProfile)=>{    
-    return res;
-  }))
-}
+  register(info: any) {
+    debugger;
 
-updateUserProfile(info:IProfile){
-  return this.http.put<IProfile>(this.baseurl+'api/updateUserProfile',info).pipe(map((res:IProfile)=>{    
-    return res;
-  }))
-}
+    return this.http.post<any>(this.baseurl + "api/register", info).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+  registerEnterprise(info: any) {
+    debugger;
+    return this.http
+      .post<any>(this.baseurl + "api/registerEnterprise", info)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+  }
 
-getEmployerProfile(info:string){
-  return this.http.get<IProfile>(this.baseurl+'api/getProfile/'+info).pipe(map((res:IProfile)=>{    
-    return res;
-  }))
-}
+  emailVerification(info: any) {
+    return this.http
+      .put<any>(this.baseurl + "api/emailVerification", info)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+  }
 
-getRecruiterProfile(info:string){
-  return this.http.get<IProfile>(this.baseurl+'api/getProfile/'+info).pipe(map((res:IProfile)=>{    
-    return res;
-  }))
-}
+  getUserData() {
+    this.user = JSON.parse(localStorage.getItem("currentUser"));
+    if (this.user) {
+      return this.user.userInfo;
+    } else {
+      return "no";
+    }
+  }
 
-forgotPassword(email:string){
-  return this.http.get<any>(this.baseurl+'api/forgotPassword/'+email).pipe(map((res:any)=>{    
-    return res;
-  }))
-}
+  getUser() {
+    this.user = JSON.parse(localStorage.getItem("currentUser"));
+    if (this.user) {
+      return this.user;
+    } else {
+      return "no";
+    }
+  }
 
-checkForgotToken(key){
-  return this.http.get<any>(this.baseurl+'api/checkForgotToken/'+key).pipe(map((res:any)=>{    
-    return res;
-  }))
-}
+  getIUserData(): IUser {
+    this.user = JSON.parse(localStorage.getItem("currentUser"));
+    if (this.user) {
+      return this.user.userInfo;
+    } else {
+      return null;
+    }
+  }
 
-resetPassword(data){
-  return this.http.put<any>(this.baseurl+'api/resetPassword',data).pipe(map((res:any)=>{
-    return res;
-  }));
-}
+  getUserProfile(userRole) {
+    return this.http
+      .get<IProfile>(this.baseurl + "api/getUserProfile/" + userRole)
+      .pipe(
+        map((res: IProfile) => {
+          return res;
+        })
+      );
+  }
 
-getRewardLoosePointsSummary(){
-  return this.http.get<any>(this.baseurl+'api/getRewardLoosePointsSummary/').pipe(map((res:any)=>{    
-    return res;
-  }))
-}
+  updateUserProfile(info: IProfile) {
+    return this.http
+      .put<IProfile>(this.baseurl + "api/updateUserProfile", info)
+      .pipe(
+        map((res: IProfile) => {
+          return res;
+        })
+      );
+  }
 
-getEarnreward(){
-  return this.http.get<any>(this.baseurl+'api/getEarnreward/').pipe(map((res:any)=>{    
-    return res;
-  }))
-}
-sendEmail(email:any){
-  return this.http.post<any>(this.baseurl+'api/sendOtp/',email).pipe(map((res:any)=>{
-    return res;
-  }))
- 
-}
-checkOtpEm(data:any){
-  return this.http.post<any>(this.baseurl+'api/verifyOtp/',data).pipe(map((res:any)=>{
-    return res;
-  }))
-}
-addQuestion(data:any){
-  return this.http.post<any>(this.baseurl+'api/addQuestion/',data).pipe(map((res:any)=>{
-    return res;
-  }))
-}
-userverification(data:any){
-  return this.http.post<any>(this.baseurl+'api/userVerification/',data).pipe(map((res:any)=>{
-    return res;
-  }))
-}
-getaskQuesUserId(){
-  return localStorage.getItem('askQuestionUser');
-}
-updateProfileImg(data:any){
-  return this.http.post<any>(this.baseurl+'api/updateProfileImg/',data).pipe(map((res:any)=>{
-    return res;
-  }))
-}
+  getEmployerProfile(info: string) {
+    return this.http
+      .get<IProfile>(this.baseurl + "api/getProfile/" + info)
+      .pipe(
+        map((res: IProfile) => {
+          return res;
+        })
+      );
+  }
 
+  getRecruiterProfile(info: string) {
+    return this.http
+      .get<IProfile>(this.baseurl + "api/getProfile/" + info)
+      .pipe(
+        map((res: IProfile) => {
+          return res;
+        })
+      );
+  }
 
+  forgotPassword(email: string) {
+    return this.http
+      .get<any>(this.baseurl + "api/forgotPassword/" + email)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+  }
 
+  checkForgotToken(key) {
+    return this.http
+      .get<any>(this.baseurl + "api/checkForgotToken/" + key)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+  }
 
+  resetPassword(data) {
+    return this.http.put<any>(this.baseurl + "api/resetPassword", data).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+
+  getRewardLoosePointsSummary() {
+    return this.http
+      .get<any>(this.baseurl + "api/getRewardLoosePointsSummary/")
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+  }
+
+  getEarnreward() {
+    return this.http.get<any>(this.baseurl + "api/getEarnreward/").pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+  sendEmail(email: any) {
+    return this.http.post<any>(this.baseurl + "api/sendOtp/", email).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+  checkOtpEm(data: any) {
+    return this.http.post<any>(this.baseurl + "api/verifyOtp/", data).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+  addQuestion(data: any) {
+    return this.http.post<any>(this.baseurl + "api/addQuestion/", data).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+  userverification(data: any) {
+    return this.http
+      .post<any>(this.baseurl + "api/userVerification/", data)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+  }
+  getaskQuesUserId() {
+    return localStorage.getItem("askQuestionUser");
+  }
+  updateProfileImg(data: any) {
+    return this.http
+      .post<any>(this.baseurl + "api/updateProfileImg/", data)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+  }
 }
