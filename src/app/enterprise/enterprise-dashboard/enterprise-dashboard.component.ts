63d77d13677affcf52906fe8fe9de79d66ae08d9
pritamCodevian <pritam.bhalerao@codevian.com>
2020-02-19 12:29:34 +0530

    UI and route guard added for enterprise

--- src/app/enterprise/enterprise-dashboard/enterprise-dashboard.component.ts
+++ src/app/enterprise/enterprise-dashboard/enterprise-dashboard.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "src/app/_services/authentication.service";
 @Component({
-  selector: 'app-enterprise-dashboard',
-  templateUrl: './enterprise-dashboard.component.html',
-  styleUrls: ['./enterprise-dashboard.component.css']
+  selector: "app-enterprise-dashboard",
+  templateUrl: "./enterprise-dashboard.component.html",
+  styleUrls: ["./enterprise-dashboard.component.css"]
 })
 export class EnterpriseDashboardComponent implements OnInit {
+  constructor(private authService: AuthenticationService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  logout() {
+    this.authService.logout();
   }
-
 }
