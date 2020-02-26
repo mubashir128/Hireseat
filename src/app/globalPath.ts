2917d272b68cf095c2e41da6f028b95244b605c8
pritamCodevian <pritam.bhalerao@codevian.com>
2020-02-26 11:39:15 +0530

    category listing and searching call resolved

--- src/app/globalPath.ts
+++ src/app/globalPath.ts
@@ -1,13 +1,11 @@
-
 //local path with IP address
 //export const baseUrl = 'http://192.168.100.21:5000/';
 
 //local path with ip server path
- export const baseUrl = 'http://192.168.100.16:5000/'; //ip link
+//  export const baseUrl = 'http://192.168.100.16:5000/'; //ip link
 
 //  export const baseUrl = 'http://portfolio.theaxontech.com:5002/'
-// export const baseUrl = 'http://localhost:5000/'; //local link link
-
+export const baseUrl = "http://localhost:5000/"; //local link link
 
 //live Path with aws ip
 //export const baseUrl = 'http://52.36.196.182:5000/';
