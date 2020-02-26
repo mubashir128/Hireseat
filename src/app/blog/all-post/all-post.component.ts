dd367e5eee5908309bc2f62634e8f7a3eb07a16b
pritamCodevian <pritam.bhalerao@codevian.com>
2020-02-26 18:14:50 +0530

    debouncing added for search category in all-posts

--- src/app/blog/all-post/all-post.component.ts
+++ src/app/blog/all-post/all-post.component.ts
@@ -3,6 +3,8 @@ import { BlogService } from "../../_services/blog.service";
 import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 import { Router } from "@angular/router";
 import { CompleterService, CompleterData } from "ng2-completer";
+import { debounceTime, distinctUntilChanged, map } from "rxjs/operators";
+
 declare var $: any;
 declare var Materialize: any;
 
@@ -19,12 +21,17 @@ export class AllPostComponent implements OnInit {
   catArray: any[] = [];
   limit: any = 100;
   searchCatagory: any;
+  blogPostDataLength: any;
   constructor(
     private _blogservice: BlogService,
     private formBuilder: FormBuilder,
     private router: Router,
     private completerService: CompleterService
-  ) {}
+  ) {
+    this.blogSearchForm = this.formBuilder.group({
+      category: ["", Validators.required]
+    });
+  }
 
   ngOnInit() {
     $(".modal").modal();
@@ -32,9 +39,14 @@ export class AllPostComponent implements OnInit {
     this.getAllBlogs(this.limit);
     this.createBlogSearchForm();
     // form value subscription
+    // debouncing
     // this.blogSearchForm
     //   .get("category")
-    //   .valueChanges.subscribe(res => console.log(res));
+    //   .valueChanges.pipe(debounceTime(2000), distinctUntilChanged())
+    //   .subscribe(res => {
+    //     this.seachBlog(res);
+    //     console.log(res);
+    //   });
   }
 
   getAllBlogs(limit) {
@@ -50,7 +62,8 @@ export class AllPostComponent implements OnInit {
     this._blogservice.getAllBlogPost(limit).subscribe(
       res => {
         this.blogPostData = res.data;
-        console.log(this.blogPostData);
+        console.log(this.blogPostData.length);
+        this.blogPostDataLength = this.blogPostData.length;
       },
       error => {
         console.log(error);
@@ -88,13 +101,15 @@ export class AllPostComponent implements OnInit {
     );
   }
 
-  seachBlog() {
+  seachBlog(text) {
     this.limit = 10;
+
     let temp: any = [];
     if (this.blogSearchForm.value.category == "") {
       this.getAllBlogs(this.limit);
     }
-    temp = this.blogSearchForm.value.category.split(",");
+    temp = text.split(",");
+    // temp = this.blogSearchForm.value.category.split(",");
     this._blogservice.getBlogByCategory(temp, this.limit).subscribe(
       response => {
         this.blogPostData = response.data;
