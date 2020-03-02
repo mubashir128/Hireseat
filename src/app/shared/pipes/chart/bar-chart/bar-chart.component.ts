ec4090218588aee48a3cce733c5c4f24118b41c8
Vilas Kale <developer.codevian@gmail.com>
2020-03-02 18:57:42 +0530

    integrated chart.js, changes in dashboard, updated values which displayed in bar chart and line chart, multiple values with multiple bars and line chart

--- src/app/shared/pipes/chart/bar-chart/bar-chart.component.ts
+++ src/app/shared/pipes/chart/bar-chart/bar-chart.component.ts
@@ -7,48 +7,183 @@ import { Chart } from 'chart.js';
 })
 export class BarChartComponent implements OnInit {
   BarChart = [{}];
-  constructor() { }
+  barChartData = {};
+  chartOptions: any;
+  constructor() {
+    // declairing bar chart data
+    this.barChartData = {
+      labels: [
+        "Recruiter1",
+        "Recruiter2",
+        "Recruiter3",
+        "Recruiter4",
+        "Recruiter5",
+        "Recruiter6",
+        "Recruiter7",
+        "Recruiter8",
+        "Recruiter9",
+        "Recruiter10",
+        "Recruiter11",
+        "Recruiter12",
+        "Recruiter13",
+        "Recruiter14",
+        "Recruiter15"
+      ],
+      datasets: [
+        {
+          label: "Score",
+          yAxisID: 'score',
+          backgroundColor: "#85e0e0",
+          borderColor: "#85e0e0",
+          borderWidth: 1,
+          data: [10098,
+            9000,
+            8998,
+            7888,
+            7000,
+            6050,
+            5001,
+            2000,
+            1998,
+            1500,
+            1100,
+            900,
+            800,
+            500,
+            100,
+          ]
+        },
+        {
+          label: "AVG Number of Days",
+          backgroundColor: "red",
+          borderColor: "red",
+          borderWidth: 1,
+          yAxisID: 'rest',
+          data: [3,
+            3,
+            3,
+            3,
+            4,
+            2,
+            5,
+            7,
+            8,
+            9,
+            9,
+            10,
+            15,
+            20,
+            25
+          ]
+        },
+        {
+          label: "# of Submissions",
+          backgroundColor: "gray",
+          borderColor: "gray",
+          borderWidth: 1,
+          yAxisID: 'rest',
+          data: [20,
+            18,
+            15,
+            12,
+            11,
+            6,
+            5,
+            5,
+            5,
+            4,
+            3,
+            4,
+            1
+          ]
+        },
+        {
+          label: "# of Interviews",
+          backgroundColor: "yellow",
+          borderColor: "yellow",
+          borderWidth: 1,
+          yAxisID: 'rest',
+          data: [16,
+            14,
+            11,
+            8,
+            7,
+            3,
+            2,
+            2,
+            2,
+            2,
+            1,
+            1,
+            1,
+            1,
+            0
+          ]
+        },
+        {
+          label: "# of Hires",
+          backgroundColor: "green",
+          borderColor: "green",
+          borderWidth: 1,
+          yAxisID: 'rest',
+          data: [10,
+            7,
+            6,
+            5,
+            3,
+            2,
+            1,
+            1,
+            1,
+            1,
+            1,
+            0,
+            0,
+            0,
+            0
+          ]
+        }
+      ]
+    };
+    // end of creating bar chart data
+    this.chartOptions = {
+      responsive: true,
+      legend: {
+        position: "top"
+      },
+      title: {
+        display: true,
+        text: "Chart Title"
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }, {
+          id: 'score',
+          type: 'linear',
+          position: 'left'
+        },
+        {
+          id: 'rest',
+          type: 'linear',
+          position: 'right',
+          ticks: {
+            max: 30,
+            min: 0
+          }
+        }
+        ]
+      }
+    }
+  }
 
   ngOnInit() {
     this.BarChart = new Chart('barChart', {
       type: 'bar',
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-          label: '# of Votes',
-          data: [9, 7, 3, 5, 2, 10],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        title: {
-          text: "Bar Chart",
-          display: true
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
+      data: this.barChartData,
+      options: this.chartOptions
     });
 
   }
--- src/app/shared/pipes/chart/line-chart/line-chart.component.ts
+++ src/app/shared/pipes/chart/line-chart/line-chart.component.ts
@@ -8,25 +8,68 @@ import { Chart } from 'chart.js';
 })
 export class LineChartComponent implements OnInit {
   LineChart: [{}];
-  constructor() { }
+  lineChartDataSet: any;
+  constructor() {
+    const recruiter1 = {
+      label: "Recruiter1",
+      fill: 'false',
+      lineTension: '0.2',
+      borderColor: "#85e0e0",
+      borderWidth: 1,
+      data: [5025, 6000, 7025, 8000, 9089, 10098]
+    };
+    const recruiter2 = {
+      label: "Recruiter2",
+      fill: 'false',
+      lineTension: '0.2',
+      borderColor: "#3399ff",
+      borderWidth: 1,
+      data: [8078, 8100, 8500, 9100, 9100, 9000]
+    }
+    const recruiter3 = {
+      label: "Recruiter3",
+      fill: 'false',
+      lineTension: '0.2',
+      borderColor: "#cc3399",
+      borderWidth: 1,
+      data: [7001, 7500, 8500, 9035, 9035, 8998]
+    };
+    const recruiter4 = {
+      label: "Recruiter4",
+      fill: 'false',
+      lineTension: '0.2',
+      borderColor: "#79ff4d",
+      borderWidth: 1,
+      data: [8565, 8201, 7525, 7202, 8010, 7888]
+    };
+    const recruiter5 = {
+      label: "Recruiter5",
+      fill: 'false',
+      lineTension: '0.2',
+      borderColor: "#6666ff",
+      borderWidth: 1,
+      data: [9025, 8001, 7001, 6002, 6210, 7000]
+    };
+    this.lineChartDataSet = {
+      labels: [
+        "Jan",
+        "Feb",
+        "March",
+        "April",
+        "May",
+        "June"
+      ],
+      datasets: [recruiter1, recruiter2, recruiter3, recruiter4, recruiter5]
+    };
+  }
 
   ngOnInit() {
     this.LineChart = new Chart('lineChart', {
       type: 'line',
-      data: {
-        labels: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [{
-          label: 'Number of Items Sold in Months',
-          data: [9, 7, 3, 5, 2, 10, 15, 16, 19, 3, 1, 9],
-          fill: false,
-          lineTension: 0.2,
-          borderColor: "red",
-          borderWidth: 1
-        }]
-      },
+      data: this.lineChartDataSet,
       options: {
         title: {
-          text: "Line Chart",
+          text: "Chart Title",
           display: true
         },
         scales: {
