70b4f8f3753bf53285c47554972a21d7eb60e47c
Vilas Kale <developer.codevian@gmail.com>
2020-03-11 11:11:26 +0530

    changes in spinner show up and hide also UI implementation by rahul on new bidding event, changes in global path

--- src/app/globalPath.ts
+++ src/app/globalPath.ts
@@ -3,6 +3,7 @@
 
 //local path with ip server path
 export const baseUrl = "http://localhost:5000/"; //ip link
+// export const baseUrl = "http://192.168.100.44:5000/"; //ip link
 
 // Test Server
 // export const baseUrl = "http://44.232.150.23:5002/"; //ip link
