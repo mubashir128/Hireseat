86cb2bda2cab0b4d3123fae080ce2c47adcf66a9
Pritam Bhalerao <pritam.bhalerao@steinnlabs.com>
2020-04-01 11:51:54 +0530

    pointing to loal

--- src/app/globalPath.ts
+++ src/app/globalPath.ts
@@ -2,7 +2,7 @@
 // export const baseUrl = 'http://192.168.100.21:5000/';
 
 // local path with ip server path
-// export const baseUrl = "http://localhost:5000/"; // ip link
+export const baseUrl = "http://localhost:5000/"; // ip link
 
 // Test Server
 // export const baseUrl = "http://44.232.150.23:5002/"; //ip link
@@ -15,7 +15,7 @@
 // export const baseUrl = 'http://52.36.196.182:5000/';
 
 // live Path with domain
-export const baseUrl = "https://hireseat.com:5000/";
+// export const baseUrl = "https://hireseat.com:5000/";
 
 // test server path
 // export const baseUrl = 'https://hireseat.com:5002/';
