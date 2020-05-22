6dec96f39340c0b1ab6cab50302d479e747d00ce
pritamCodevian <pritam.bhalerao@codevian.com>
2020-05-22 20:01:17 +0530

    no change

--- src/app/shared-video/shared-video.component.ts
+++ src/app/shared-video/shared-video.component.ts
@@ -4,6 +4,7 @@ import { ShareVideoService } from '../_services/share-video.service';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-shared-video',
@@ -25,10 +26,12 @@ export class SharedVideoComponent implements OnInit, OnDestroy {
     private elementRef: ElementRef,
     private sharedVideoService: ShareVideoService,
     private activatedRoute: ActivatedRoute,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private spinner: NgxSpinnerService
   ) { }
 
   ngOnInit() {
+    this.spinner.show();
     this.token = this.activatedRoute.snapshot.paramMap.get('token');
 
     this.checkSharedTokenSubscription = this.sharedVideoService.checkSharedToken(this.token).subscribe((res) => {
@@ -40,27 +43,35 @@ export class SharedVideoComponent implements OnInit, OnDestroy {
         // console.log(this.resume);
         this.videoURL = res.videoUrl;
         this.isTokenValid = true;
+        this.spinner.hide();
       }
     }, err => {
       // console.log(err, '***************************look up*********************');
       this.isTokenValid = false;
-
+      this.spinner.hide();
     });
   }
 
   ngAfterViewInit() {
     // instantiate Video.js
     if (this.videoURL && this.isTokenValid) {
+      this.spinner.show();
+
       this.player = videojs(this.target.nativeElement, {
         autoplay: true,
         controlls: true
       }, () => {
+        this.spinner.hide();
+
         // console.log('onPlayerReady', this);
       }, err => {
         console.log('*************', err);
+        this.spinner.hide();
+
       });
     } else {
       console.log('token is not valid');
+      this.spinner.hide();
 
     }
 
