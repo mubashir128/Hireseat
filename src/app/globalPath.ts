2a79590bbfdd389ed5362c7bd6b0c216bd00f6ef
pritamCodevian <pritam.bhalerao@codevian.com>
2020-06-01 15:09:18 +0530

    video calling pulled

--- src/app/globalPath.ts
+++ src/app/globalPath.ts
@@ -2,7 +2,7 @@
 // export const baseUrl = 'http://192.168.100.21:5000/';
 
 // local path with ip server path
-export const baseUrl = 'http://localhost:5000/'; // ip link
+export const baseUrl = 'http://localhost:5002/'; // ip link
 // export const SAMPLE_SERVER_BASE_URL = 'https://tokboxhireseat.herokuapp.com/'; // Sujith
 
 // Test Server
