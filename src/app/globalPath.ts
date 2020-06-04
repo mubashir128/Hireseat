f414c6e95c85fe70bf225ff5535fef2a9a0964b9
a121 <shankar.pisal@codevian.com>
2020-06-04 21:18:06 +0530

    move socket url to global path.

--- src/app/globalPath.ts
+++ src/app/globalPath.ts
@@ -3,21 +3,32 @@
 
 // local path with ip server path
 export const baseUrl = 'http://localhost:5000/'; // ip link
+// socketUrl
+export const socketUrl = "ws://localhost:5000/webSocket?token=";
 // export const SAMPLE_SERVER_BASE_URL = 'https://tokboxhireseat.herokuapp.com/'; // Sujith
 
 // Test Server
 // export const baseUrl = "http://44.232.150.23:5002/"; //ip link
+// socketUrl
+// export const socketUrl = "wss://44.232.150.23:5002/webSocket?token=";
 
 //  export const baseUrl = 'http://portfolio.theaxontech.com:5002/'
 // export const baseUrl = "http://localhost:5000/"; //local link link
 // export const baseUrl = "http://192.168.100.44:5000/"; //local link link
+// socketUrl
+// export const socketUrl = "wss://192.168.100.44:5000/webSocket?token=";
 
 // live Path with aws ip
 // export const baseUrl = 'http://52.36.196.182:5000/';
-
+// socketUrl
+// export const socketUrl = "wss://52.36.196.182:5000/webSocket?token=";
 
 // live Path with domain
 // export const baseUrl = 'https://hireseat.com:5000/';
+// socketUrl
+// export const socketUrl = "wss://hireseat.com:5000/webSocket?token=";
 
 // test server path
 // export const baseUrl = 'https://hireseat.com:5002/';
+// socketUrl
+// export const socketUrl = "wss://hireseat.com:5002/webSocket?token=";
