c0df0a3ef844f2eabeb5868ccb3eaf9531ae31c3
Pritam Bhalerao <pritam.bhalerao@codevian.com>
2020-08-18 20:32:23 +0530

    no change

--- src/app/globalPath.ts
+++ src/app/globalPath.ts
@@ -5,7 +5,7 @@
 export const baseUrl = 'http://localhost:5002/'; // ip link
 // socketUrl
 export const socketUrl = "ws://localhost:5002/webSocket";
-// export const SAMPLE_SERVER_BASE_URL = 'https://tokboxhireseat.herokuapp.com/'; // Sujith
+// export const SAMPLE_SERVER_BASE_URL = 'https://tokboxhireseat.herokuapp.com/';  Sujith
 
 // Test Server
 // export const baseUrl = "http://44.232.150.23:5002/"; //ip link
