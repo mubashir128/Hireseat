ccf1230ed57222718a4c2149b50f7b7716b3e77e
Bhalerao Pritam Nitin <pritam.bhalerao@codevian.com>
2020-10-13 15:35:31 +0530

    changes in port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://35.162.247.138:5000/',
-  socketUrl: "http://35.162.247.138:5000/webSocket"
+  baseUrl: 'http://35.162.247.138:8000/',
+  socketUrl: "http://35.162.247.138:8000/webSocket"
 };
