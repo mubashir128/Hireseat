c2d11fe8554aae900d3ea07996ef23f563e09e67
pritam codevian <pritam.bhalerao@codevian.com>
2021-01-13 10:32:53 +0530

    y axis

--- src/app/recruiter/charts/recruiter-pie-chart/recruiter-pie-chart.component.ts
+++ src/app/recruiter/charts/recruiter-pie-chart/recruiter-pie-chart.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Chart } from "chart.js";
-import { WebsocketService } from 'src/app/_services/websocket.service';
+import { WebsocketService } from "src/app/_services/websocket.service";
 import { Subject } from "rxjs";
 import { UserService } from "../../../_services/user.service";
 
 @Component({
-  selector: 'app-recruiter-pie-chart',
-  templateUrl: './recruiter-pie-chart.component.html',
-  styleUrls: ['./recruiter-pie-chart.component.css']
+  selector: "app-recruiter-pie-chart",
+  templateUrl: "./recruiter-pie-chart.component.html",
+  styleUrls: ["./recruiter-pie-chart.component.css"],
 })
 export class RecruiterPieChartComponent implements OnInit {
   PieChart;
@@ -19,24 +19,27 @@ export class RecruiterPieChartComponent implements OnInit {
   recruiterPieChartObserver$ = this.recruiterPieChartObserver.asObservable();
 
   loggedInUser: any;
-  constructor(private _socket: WebsocketService, private userService: UserService) {
+  constructor(
+    private _socket: WebsocketService,
+    private userService: UserService
+  ) {
     this.setPieChartConfig();
     this.loggedInUser = this.userService.getUserData();
   }
 
   async ngOnInit() {
     this.showPieChart();
-    
+
     await this._socket.removeListener({ type: 7 });
     this._socket.addListener({
       type: 7,
       callback: this.recruiterPieChartObserver,
     });
-    
+
     this.recruiterPieChartObserver$.subscribe((res: any) => {
       this.handleRecruiterPieChartData(res);
     });
-    
+
     this._socket.sendMessage({
       type: 7,
       data: {
@@ -45,17 +48,30 @@ export class RecruiterPieChartComponent implements OnInit {
       },
     });
   }
-  
-  setPieChartConfig(){
+
+  setPieChartConfig() {
     this.pieChartDataSet = {
-      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
       datasets: [
         {
           label: "Candidate helped / month.",
           backgroundColor: "#0aafff",
           // borderColor: "#0aafff",
           borderWidth: 1,
-          data: [0, 0, 0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0]
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         },
         // {
         //   label: "% Number of comments / month.",
@@ -64,28 +80,28 @@ export class RecruiterPieChartComponent implements OnInit {
         //   borderWidth: 1,
         //   data: [0, 0, 0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0]
         // }
-      ]
+      ],
     };
   }
 
-  handleRecruiterPieChartData(res){
+  handleRecruiterPieChartData(res) {
     switch (res.subType) {
-      case this.getRecruiterPieChartData : 
+      case this.getRecruiterPieChartData:
         this.putDataIntoFirstPieBar(res);
         break;
-      default : 
+      default:
         break;
     }
   }
 
-  putDataIntoFirstPieBar(res){
+  putDataIntoFirstPieBar(res) {
     this.pieChartDataSet.datasets[0].data = res.data;
     // this.pieChartDataSet.datasets[1].data = [(res.data[0]/res.monthRange[0]) * 100, (res.data[1]/res.monthRange[1]) * 100, (res.data[2]/res.monthRange[2]) * 100, (res.data[3]/res.monthRange[3]) * 100, (res.data[4]/res.monthRange[4]) * 100, (res.data[5]/res.monthRange[5]) * 100, (res.data[6]/res.monthRange[6]) * 100, (res.data[7]/res.monthRange[7]) * 100, (res.data[8]/res.monthRange[8]) * 100, (res.data[9]/res.monthRange[9]) * 100, (res.data[10]/res.monthRange[10]) * 100, (res.data[11]/res.monthRange[11]) * 100];
-    
+
     this.PieChart.update();
   }
 
-  showPieChart(){
+  showPieChart() {
     this.PieChart = new Chart("PieChart", {
       type: "bar",
       data: this.pieChartDataSet,
@@ -96,32 +112,37 @@ export class RecruiterPieChartComponent implements OnInit {
             // fontFamily: "Comic Sans MS",
             boxWidth: 8,
             boxHeight: 0,
-            fontSize: 10
+            fontSize: 10,
           },
           maxSize: {
-            height: 0
-          }
+            height: 0,
+          },
         },
         title: {
           text: "Candidate helped / month.",
-          display: true
+          display: true,
         },
         scales: {
           yAxes: [
             {
               ticks: {
-                beginAtZero: true
+                beginAtZero: true,
+                callback: function (value) {
+                  if (value % 1 === 0) {
+                    return value;
+                  }
+                },
               },
               gridLines: {
                 color: "rgba(0, 0, 0, 0)",
-                display: true
-              }
+                display: true,
+              },
             },
             // {
             //   id: "percent",
             //   type: "linear",
             //   position: "right",
-  
+
             //   ticks: {
             //     max: 100,
             //     min: 0,
@@ -137,18 +158,17 @@ export class RecruiterPieChartComponent implements OnInit {
           xAxes: [
             {
               ticks: {
-                beginAtZero: true
+                beginAtZero: true,
               },
               gridLines: {
                 color: "rgba(0, 0, 0, 0)",
-                display: false
-              }
-            }
-          ]
+                display: false,
+              },
+            },
+          ],
         },
-        bezierCurve: false
-      }
+        bezierCurve: false,
+      },
     });
   }
-
 }
