f7b275d02ca51cdcb74159ce280867b9df7d26ce
a121 <shankar.pisal@codevian.com>
2021-02-09 12:40:19 +0530

    added a changes socket.

--- src/app/_services/websocket.service.ts
+++ src/app/_services/websocket.service.ts
@@ -36,46 +36,23 @@ export class WebsocketService {
     this.socket.on("message", (data: any) => {
       this.onMessage(data);
     });
-    this.socket.on("comment", (data: any) => {
-      console.log("-------------", data);
-      this.onComment(data);
-    });
+
     this.socket.on("disconnect", (data: any) => {
       this.onClose(data);
     });
-    // this.getProfiles();
   }
 
   private onOpen(obj: any) {
     // console.log("connection opend : ", obj);
   }
-  // getProfiles() {
-  //   // console.log("getting data before check", this.socket);
 
-  //   if (this.socket !== undefined) {
-  //     return new Observable((subscriber) => {
-  //       this.socket.on("sharedCandidateProfile", (data: any) => {
-  //         // console.log("**************socket*****************", data);
-  //         subscriber.next(data);
-  //       });
-  //     });
-  //     // console.log("getting data");
-  //   }
-  // }
   sendMessage(obj: any) {
     if (this.socket !== undefined) {
       // console.log("sending a msg", obj);
-
       this.socket.emit("message", JSON.stringify(obj));
     }
   }
-  getProfiles(obj: any) {
-    if (this.socket !== undefined) {
-      console.log("sending signal to comments", obj);
 
-      this.socket.emit("comment", JSON.stringify(obj));
-    }
-  }
   private onMessage(obj: string) {
     let res = JSON.parse(obj);
     // console.log("message received : ", res);
@@ -85,22 +62,13 @@ export class WebsocketService {
       }
     });
   }
-  private onComment(obj: string) {
-    let res = JSON.parse(obj);
-    console.log("Comment ------- : ", res);
-    this.listeners.forEach((key, index) => {
-      if (res.type === key.type) {
-        key.callback.next(res.data);
-      }
-    });
-  }
+  
   private onClose(obj: any) {
     //  console.log("connection closed : ",obj);
   }
 
   socketClosed() {
     console.log("closing socket");
-
     this.socket.close();
     this.socket = undefined;
   }
