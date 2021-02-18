60f8059d3e9e307953e63c480bb4ba733898605d
a121 <shankar.pisal@codevian.com>
2021-02-18 15:35:47 +0530

    added a changes for constants.

--- src/app/_services/constants.service.ts
+++ src/app/_services/constants.service.ts
@@ -1,63 +1,195 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import * as myGlobals from "../globalPath";
 @Injectable({
   providedIn: 'root'
 })
 export class ConstantsService {
 
-  ratingPoints = 1000;
-  sharedPoints = 200;
-  ReplyAdvicePoints = 25;
-  adviceLikedPoints = 50;
-  advicePoints = 100;
-  notificationLimit = 15;
+  // ratingPoints = 1000;
+  // sharedPoints = 200;
+  // ReplyAdvicePoints = 25;
+  // adviceLikedPoints = 50;
+  // advicePoints = 100;
+  // notificationLimit = 15;
 
-  notificationType = 1;
-  getAllNotifications = "getAllNotifications";
-  newNotification = "newNotification";
-  decreaseNotificationCount = "decreaseNotificationCount";
+  // notificationType = 1;
+  // getAllNotifications = "getAllNotifications";
+  // newNotification = "newNotification";
+  // decreaseNotificationCount = "decreaseNotificationCount";
 
-  profileQuestionType = 3;
-  getAllQuestions = "getAllQuestions";
-  question = "question";
-  answer = "answer";
+  // profileQuestionType = 3;
+  // getAllQuestions = "getAllQuestions";
+  // question = "question";
+  // answer = "answer";
 
-  recruiterDoughnutChartType = 6;
-  getDoughnutChartData = "getDoughnutChartData";
-  getAllRecruiterComment = "getAllRecruiterComment";
+  // recruiterDoughnutChartType = 6;
+  // getDoughnutChartData = "getDoughnutChartData";
+  // getAllRecruiterComment = "getAllRecruiterComment";
 
-  recruiterLineChartType = 8;
-  getRecruiterLineChartData = "getRecruiterLineChartData";
+  // recruiterLineChartType = 8;
+  // getRecruiterLineChartData = "getRecruiterLineChartData";
 
-  recruiterPieChartType = 7;
-  getRecruiterPieChartData = "getRecruiterPieChartData";
+  // recruiterPieChartType = 7;
+  // getRecruiterPieChartData = "getRecruiterPieChartData";
 
-  sharedProfileType = 5;
-  getAllSharedProfiles = "getAllSharedProfiles";
-  addComment = "addComment";
-  likeComment = "likeComment";
-  shareVideoViaRecruiterEmail = "shareVideoViaRecruiterEmail";
-  editComment = "editComment";
-  deleteComment = "deleteComment";
-  replyComment = "replyComment";
+  // sharedProfileType = 5;
+  // getAllSharedProfiles = "getAllSharedProfiles";
+  // addComment = "addComment";
+  // likeComment = "likeComment";
+  // shareVideoViaRecruiterEmail = "shareVideoViaRecruiterEmail";
+  // editComment = "editComment";
+  // deleteComment = "deleteComment";
+  // replyComment = "replyComment";
 
-  barChartType = 4;
-  getAllBarChartData = "getAllBarChartData";
-  increaseRatingPoints = "increaseRatingPoints";
-  increaseHiredCount = "increaseHiredCount";
-  pushNewCreatedBid = "pushNewCreatedBid";
+  // barChartType = 4;
+  // getAllBarChartData = "getAllBarChartData";
+  // increaseRatingPoints = "increaseRatingPoints";
+  // increaseHiredCount = "increaseHiredCount";
+  // pushNewCreatedBid = "pushNewCreatedBid";
   
-  askQuestionType = 2;
-  getQuestion = "getQuestion";
+  // askQuestionType = 2;
+  // getQuestion = "getQuestion";
 
-  private = "private";
-  public = "public";
-  all = "all";
+  // private = "private";
+  // public = "public";
+  // all = "all";
 
-  sharedCandidateProfileType = 10;
+  // sharedCandidateProfileType = 10;
   
-  sharedRecruiterProfileType = 9;
-  candidateJobQuestionType = 11;
-  constructor() { }
+  // sharedRecruiterProfileType = 9;
+  // candidateJobQuestionType = 11;
+
+  getAllNotifications;
+  newNotification;
+  notificationLimit;
+  getQuestion;
+  getAllQuestions;
+  askedQuestion;
+  answer;
+  question;
+  postAnswer;
+  getAllBarChartData;
+  increaseRatingPoints;
+  increaseHiredCount;
+  pushNewCreatedBid;
+  getAllSharedProfiles;
+  addComment;
+  likeComment;
+  replyAdvicePoints;
+  getDoughnutChartData;
+  getRecruiterPieChartData;
+  getRecruiterLineChartData;
+  getAllRecruiterComment;
+  shareVideoViaRecruiterEmail;
+  comment;
+  reply;
+  like;
+  askQuestion;
+  profileQuestion;
+  profileAnswer;
+  decreaseNotificationCount;
+  shareCandidateProfile;
+  editComment;
+  deleteComment;
+  replyComment;
+  ratingPoints;
+  sharedPoints;
+  ReplyAdvicePoints;
+  adviceLikedPoints;
+  advicePoints;
+  notificationType;
+  profileQuestionType;
+  recruiterDoughnutChartType;
+  recruiterLineChartType;
+  recruiterPieChartType;
+  sharedProfileType;
+  barChartType;
+  askQuestionType;
+  id;
+  employerKey;
+  recruiterKey;
+  private;
+  public;
+  all;
+  ACTIVE;
+  SCHEDULED;
+  recruiterComment;
+  sharedCandidateProfileType;
+  sharedRecruiterProfileType;
+  candidateJobQuestionType;
+
+  public baseurl: any;
+  constructor(private http: HttpClient) {
+    this.baseurl = myGlobals.baseUrl;
+  }
+
+  getAllConstants(){
+    this.http.get<any>(this.baseurl + "api/getAllConstants").subscribe(res=>{
+      this.setVariables(res);
+    });
+  }
+  
+  setVariables(obj){
+    this.getAllNotifications = obj.getAllNotifications;
+    this.newNotification = obj.newNotification;
+    this.getQuestion = obj.addQuestion;
+    this.getAllQuestions = obj.getAllQuestions;
+    this.askedQuestion = obj.askedQuestion;
+    this.answer = obj.answer;
+    this.question = obj.question;
+    this.postAnswer = obj.postAnswer;
+    this.getAllBarChartData = obj.getAllBarChartData;
+    this.increaseRatingPoints = obj.increaseRatingPoints;
+    this.increaseHiredCount = obj.increaseHiredCount;
+    this.pushNewCreatedBid = obj.pushNewCreatedBid;
+    this.getAllSharedProfiles = obj.getAllSharedProfiles;
+    this.addComment = obj.addComment;
+    this.likeComment = obj.likeComment;
+    this.replyAdvicePoints = obj.replyAdvicePoints;
+    this.getDoughnutChartData = obj.getDoughnutChartData;
+    this.getRecruiterPieChartData = obj.getRecruiterPieChartData;
+    this.getRecruiterLineChartData = obj.getRecruiterLineChartData;
+    this.getAllRecruiterComment = obj.getAllRecruiterComment;
+    this.shareVideoViaRecruiterEmail = obj.shareVideoViaRecruiterEmail;
+    this.comment = obj.comment;
+    this.reply = obj.reply;
+    this.like = obj.like;
+    this.askQuestion = obj.askQuestion;
+    this.profileQuestion = obj.profileQuestion;
+    this.profileAnswer = obj.profileAnswer;
+    this.decreaseNotificationCount = obj.decreaseNotificationCount;
+    this.shareCandidateProfile = obj.shareCandidateProfile;
+    this.editComment = obj.editComment;
+    this.deleteComment = obj.deleteComment;
+    this.replyComment = obj.replyComment;
+    this.ratingPoints = obj.ratingPoints;
+    this.sharedPoints = obj.sharedPoints;
+    this.ReplyAdvicePoints = obj.ReplyAdvicePoints;
+    this.adviceLikedPoints =  obj.adviceLikedPoints;
+    this.advicePoints = obj.advicePoints;
+    this.notificationType = obj.notificationType;
+    this.profileQuestionType = obj.profileQuestionType;
+    this.recruiterDoughnutChartType = obj.recruiterDoughnutChartType,
+    this.recruiterLineChartType = obj.recruiterLineChartType;
+    this.recruiterPieChartType = obj.recruiterPieChartType;
+    this.sharedProfileType = obj.sharedProfileType;
+    this.barChartType = obj.barChartType;
+    this.askQuestionType = obj.askQuestionType;
+    this.id = obj.id;
+    this.employerKey = obj.employerKey;
+    this.recruiterKey = obj.recruiterKey;
+    this.private = obj.private;
+    this.public = obj.public;
+    this.all = obj.all;
+    this.ACTIVE = obj.ACTIVE;
+    this.SCHEDULED = obj.SCHEDULED;
+    this.recruiterComment = obj.recruiterComment;
+    this.sharedCandidateProfileType = obj.sharedCandidateProfileType;
+    this.sharedRecruiterProfileType = obj.sharedRecruiterProfileType;
+    this.candidateJobQuestionType = obj.candidateJobQuestionType;
+    this.notificationLimit = obj.notificationLimit;
+  }
+
 }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ConstantsService } from "src/app/_services/constants.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private _constants : ConstantsService){
+    this._constants.getAllConstants();
+  }
 }
