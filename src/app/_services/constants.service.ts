b8228934e640334b1bc1a742d49590eb70c03d24
a121 <shankar.pisal@codevian.com>
2021-02-22 18:42:10 +0530

    added a some constants changes.

--- src/app/_services/constants.service.ts
+++ src/app/_services/constants.service.ts
@@ -6,60 +6,6 @@ import * as myGlobals from "../globalPath";
 })
 export class ConstantsService {
 
-  // ratingPoints = 1000;
-  // sharedPoints = 200;
-  // ReplyAdvicePoints = 25;
-  // adviceLikedPoints = 50;
-  // advicePoints = 100;
-  // notificationLimit = 15;
-
-  // notificationType = 1;
-  // getAllNotifications = "getAllNotifications";
-  // newNotification = "newNotification";
-  // decreaseNotificationCount = "decreaseNotificationCount";
-
-  // profileQuestionType = 3;
-  // getAllQuestions = "getAllQuestions";
-  // question = "question";
-  // answer = "answer";
-
-  // recruiterDoughnutChartType = 6;
-  // getDoughnutChartData = "getDoughnutChartData";
-  // getAllRecruiterComment = "getAllRecruiterComment";
-
-  // recruiterLineChartType = 8;
-  // getRecruiterLineChartData = "getRecruiterLineChartData";
-
-  // recruiterPieChartType = 7;
-  // getRecruiterPieChartData = "getRecruiterPieChartData";
-
-  // sharedProfileType = 5;
-  // getAllSharedProfiles = "getAllSharedProfiles";
-  // addComment = "addComment";
-  // likeComment = "likeComment";
-  // shareVideoViaRecruiterEmail = "shareVideoViaRecruiterEmail";
-  // editComment = "editComment";
-  // deleteComment = "deleteComment";
-  // replyComment = "replyComment";
-
-  // barChartType = 4;
-  // getAllBarChartData = "getAllBarChartData";
-  // increaseRatingPoints = "increaseRatingPoints";
-  // increaseHiredCount = "increaseHiredCount";
-  // pushNewCreatedBid = "pushNewCreatedBid";
-  
-  // askQuestionType = 2;
-  // getQuestion = "getQuestion";
-
-  // private = "private";
-  // public = "public";
-  // all = "all";
-
-  // sharedCandidateProfileType = 10;
-  
-  // sharedRecruiterProfileType = 9;
-  // candidateJobQuestionType = 11;
-
   getAllNotifications;
   newNotification;
   notificationLimit;
