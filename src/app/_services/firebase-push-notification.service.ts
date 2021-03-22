8df668e97d1aa30295263da1284e8e84497a2016
a121 <shankar.pisal@codevian.com>
2021-03-22 15:09:47 +0530

    added a changes for toast

--- src/app/_services/firebase-push-notification.service.ts
+++ src/app/_services/firebase-push-notification.service.ts
@@ -21,8 +21,6 @@ export class FirebasePushNotificationService {
   }
   
   initiate(){
-    // this.requestPermission();
-    Materialize.toast("--- calling method this.requestAndroidDevicePermission() : ", 3000, "blue");
     this.requestAndroidDevicePermission();
   }
 
@@ -30,16 +28,13 @@ export class FirebasePushNotificationService {
     if(FirebasePushNotificationService.push){
       return ;
     }
-    Materialize.toast("--- PushNotifications.requestPermission() : "+FirebasePushNotificationService.push, 3000, "blue");
+
     PushNotifications.requestPermission().then( result => {
-      Materialize.toast("result.granted : "+result.granted, 3000, "blue");
       if (result.granted) {
         // Register with Apple / Google to receive push via APNS/FCM
-        Materialize.toast("true result.granted : "+result.granted, 3000, "blue");
         PushNotifications.register();
       } else {
         // Show some error
-        Materialize.toast("false result.granted : "+result.granted, 3000, "blue");
       }
     }).catch(err=>{
       Materialize.toast("--- error is : "+err, 3000, "blue");
@@ -47,7 +42,6 @@ export class FirebasePushNotificationService {
 
     // On success, we should be able to receive notifications
     PushNotifications.addListener('registration', (token: PushNotificationToken) => {
-      Materialize.toast("Push registration success, token : "+token.value, 3000, "blue");
       let loggedInUser = JSON.parse(localStorage.getItem('currentUser'));
       this.token = token.value;
       let payload = {
@@ -60,44 +54,16 @@ export class FirebasePushNotificationService {
 
     // Some issue with our setup and push will not work
     PushNotifications.addListener('registrationError', (error: any) => {
-      Materialize.toast("Error on registration : "+JSON.stringify(error), 3000, "blue");
     });
 
     // Show us the notification payload if the app is open on our device
     PushNotifications.addListener('pushNotificationReceived', (notification: PushNotification) => {
-      Materialize.toast("Error on registration : "+JSON.stringify(notification), 3000, "blue");
     });
 
     // Method called when tapping on a notification
     PushNotifications.addListener('pushNotificationActionPerformed', (notification: PushNotificationActionPerformed) => {
-      Materialize.toast("Push action performed : "+JSON.stringify(notification), 3000, "blue");
     });
-  }
-
-  requestPermission(){
-    if(FirebasePushNotificationService.push){
-      return ;
-    }
-
-    let loggedInUser = JSON.parse(localStorage.getItem('currentUser'));
-    this.angularFireMessaging.requestToken.subscribe(token=>{
-        this.token = token;
-        let payload = {
-          pushToken : this.token,
-          userToken : loggedInUser.token,
-          userRole : loggedInUser.userInfo.userRole
-        }
-        this.receiveMessage();
-        this.openConnection(payload);
-      },(err)=>{
-        console.log("err : ",err);
-      });
-  }
-
-  receiveMessage() {
-    this.angularFireMessaging.messages.subscribe((payload : any) => {
-      this.currentMessage.next(payload);
-    });
+    
   }
 
   closeFirebasePushNotification() {
