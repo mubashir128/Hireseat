75e46b721ef7c788e490b02be2809e65a195d381
pritam codevian <pritam.bhalerao@codevian.com>
2021-03-25 21:12:13 +0530

    toaster removed on push notification error, styles invoke for recruiter, Login button on home screen for mobile view

--- src/app/_services/firebase-push-notification.service.ts
+++ src/app/_services/firebase-push-notification.service.ts
@@ -1,90 +1,101 @@
 import { HttpClient } from "@angular/common/http";
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 import * as myGlobals from "../globalPath";
 declare var Materialize: any;
-import {Plugins, PushNotification, PushNotificationToken, PushNotificationActionPerformed} from '@capacitor/core';
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed,
+} from "@capacitor/core";
 const { PushNotifications } = Plugins;
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FirebasePushNotificationService {
-
   token;
   static push = false;
   pushNotifyUrl = "api/firebasePushNotification";
-  closePushNotifyUrl = "api/closeFirebasePushNotification"
+  closePushNotifyUrl = "api/closeFirebasePushNotification";
   currentMessage = new BehaviorSubject({});
 
-  constructor(private _http: HttpClient) {
-  }
-  
-  initiate(){
+  constructor(private _http: HttpClient) {}
+
+  initiate() {
     //for ios.
     this.requestIosDevicePermission();
   }
 
-  requestIosDevicePermission(){
-    if(FirebasePushNotificationService.push){
-      return ;
+  requestIosDevicePermission() {
+    if (FirebasePushNotificationService.push) {
+      return;
     }
 
-    PushNotifications.requestPermission().then( result => {
-      if (result.granted) {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    }).catch(err=>{
-      Materialize.toast("--- error is : "+err, 3000, "blue");
-    });
+    PushNotifications.requestPermission()
+      .then((result) => {
+        if (result.granted) {
+          // Register with Apple / Google to receive push via APNS/FCM
+          PushNotifications.register();
+        } else {
+          // Show some error
+        }
+      })
+      .catch((err) => {
+        // Materialize.toast("--- error is : " + err, 3000, "blue");
+      });
 
     // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration', (token: PushNotificationToken) => {
-      let loggedInUser = JSON.parse(localStorage.getItem('currentUser'));
-      this.token = token.value;
-      let payload = {
-        deviceType : "ios",
-        pushToken : this.token,
-        userToken : loggedInUser.token,
-        userRole : loggedInUser.userInfo.userRole
+    PushNotifications.addListener(
+      "registration",
+      (token: PushNotificationToken) => {
+        let loggedInUser = JSON.parse(localStorage.getItem("currentUser"));
+        this.token = token.value;
+        let payload = {
+          deviceType: "ios",
+          pushToken: this.token,
+          userToken: loggedInUser.token,
+          userRole: loggedInUser.userInfo.userRole,
+        };
+        this.openConnection(payload);
       }
-      this.openConnection(payload);
-    });
+    );
 
     // Some issue with our setup and push will not work
-    PushNotifications.addListener('registrationError', (error: any) => {
-    });
+    PushNotifications.addListener("registrationError", (error: any) => {});
 
     // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotification) => {
-    });
+    PushNotifications.addListener(
+      "pushNotificationReceived",
+      (notification: PushNotification) => {}
+    );
 
     // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed', (notification: PushNotificationActionPerformed) => {
-    });
-    
+    PushNotifications.addListener(
+      "pushNotificationActionPerformed",
+      (notification: PushNotificationActionPerformed) => {}
+    );
   }
 
   closeFirebasePushNotification() {
-    let loggedInUser = JSON.parse(localStorage.getItem('currentUser'));
+    let loggedInUser = JSON.parse(localStorage.getItem("currentUser"));
     let payload = {
-      pushToken : this.token,
-      userToken : loggedInUser.token,
-      userRole : loggedInUser.userInfo.userRole
-    }
+      pushToken: this.token,
+      userToken: loggedInUser.token,
+      userRole: loggedInUser.userInfo.userRole,
+    };
     this.closeConnection(payload);
   }
 
-  openConnection(payload){
-    this._http.post(myGlobals.baseUrl+""+this.pushNotifyUrl, payload).subscribe(res=>{
-      FirebasePushNotificationService.push = true;
-    });
+  openConnection(payload) {
+    this._http
+      .post(myGlobals.baseUrl + "" + this.pushNotifyUrl, payload)
+      .subscribe((res) => {
+        FirebasePushNotificationService.push = true;
+      });
   }
 
-  closeConnection(payload){
+  closeConnection(payload) {
     FirebasePushNotificationService.push = false;
   }
-
 }
