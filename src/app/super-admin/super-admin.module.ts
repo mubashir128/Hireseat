dd82514558c3807216268e3cb9d77728e5ef6d0f
a121 <shankar.pisal@codevian.com>
2021-04-05 18:05:11 +0530

    added a sharedModule to superAdminModule

--- src/app/super-admin/super-admin.module.ts
+++ src/app/super-admin/super-admin.module.ts
@@ -4,17 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SuperAdminRoutingModule } from './super-admin-routing.module';
 import { SADashboardComponent } from './sa-dashboard/sa-dashboard.component';
 import { SANavbarComponent } from './sa-navbar/sa-navbar.component';
-import { MobileNavTabComponent } from '../shared/shared-components/components/mobile-nav-tab/mobile-nav-tab.component';
+import { SharedComponentsModule } from '../shared/shared-components/shared-components.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    SuperAdminRoutingModule
+    SuperAdminRoutingModule,
+    SharedComponentsModule
   ],
   declarations: [
     SADashboardComponent,
     SANavbarComponent,
-    MobileNavTabComponent
   ]
 })
 export class SuperAdminModule { }
