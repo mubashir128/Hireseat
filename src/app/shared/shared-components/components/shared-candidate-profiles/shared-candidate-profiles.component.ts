13c5a8f938f05fc79d6360d49699e8d4788bfca6
a121 <shankar.pisal@codevian.com>
2021-04-15 11:04:16 +0530

    added a email validation

--- src/app/shared/shared-components/components/shared-candidate-profiles/shared-candidate-profiles.component.ts
+++ src/app/shared/shared-components/components/shared-candidate-profiles/shared-candidate-profiles.component.ts
@@ -674,6 +674,14 @@ export class SharedCandidateProfilesComponent
   }
 
   async share() {
+
+    var EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!EMAIL_REGEXP.test(this.recipientEmail)) {
+      Materialize.toast("Invalid email", 800);
+      return;
+    }
+
     jQuery("#shareEmailModal").modal("close");
     this.spinner.show();
 
