9db8403b99318419a2c9496fcd680490b7a88e60
a121 <shankar.pisal@codevian.com>
2021-06-02 14:28:28 +0530

    fillForm

---
+++ src/app/shared/shared-components/components/fill-form/fill-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fill-form',
+  templateUrl: './fill-form.component.html',
+  styleUrls: ['./fill-form.component.css']
+})
+export class FillFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("--- form fill : ");
+  }
+
+}
--- src/app/shared/shared-components/components/menus/menus.component.ts
+++ src/app/shared/shared-components/components/menus/menus.component.ts
@@ -97,6 +97,8 @@ export class MenusComponent implements OnInit {
     this.tabs2.push(new Tab2("/candidate/all-recruiters", "Recruiters", false, "fas fa-user-alt"));
     this.tabs2.push(new Tab2("/blog", "Blog", false, "fas fa-bell"));
 
+    this.tabs2.push(new Tab2("/candidate/form-fill", "FormFill", false, "fas fa-shopping-bag"));
+
     this.tabs2.push(new Tab2("/candidate/my-reviewed-profiles", "My Reviews Profiles", false, "fas fa-shopping-bag"));
     this.tabs2.push(new Tab2("/candidate/interview-room", "Interview Room", false, "fas fa-question"));
     this.tabs2.push(new Tab2("/forum", "Ask a Recruiter", false, "fas fa-shopping-bag"));
