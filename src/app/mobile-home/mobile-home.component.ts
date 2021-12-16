0a09a20aa862a39036aea991f1d4ff58c6ff937e
shankarpisal <spisal@mrccitsolutions.com>
2021-12-16 16:19:04 +0530

    changes mobil-home btn

--- src/app/mobile-home/mobile-home.component.ts
+++ src/app/mobile-home/mobile-home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { App } from '@capacitor/app';
+import { UserService } from "../_services/user.service";
+import { Router, ActivatedRoute } from "@angular/router";
+
 declare var jQuery:any;
 
 @Component({
@@ -11,7 +14,19 @@ export class MobileHomeComponent implements OnInit {
   appInfo;
   currVersion = '1.0.0';
 
-  constructor() { }
+  loggedInUser : any;
+  btnName = "Login";
+
+  constructor(private _userservice: UserService, private router: Router) {
+    this.loggedInUser = this._userservice.getUserData();
+    if (this.loggedInUser != "no") {
+      this.btnName = "Enter";
+    }
+  }
+
+  goToLogin() {
+    this.router.navigate(["/login"]);
+  }
 
   async ngOnInit(){
     this.appInfo = await App.getInfo();
