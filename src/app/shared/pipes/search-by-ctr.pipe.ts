ec69ee7822369d6cf0e5e86329c2e8e76d5f7688
shankarpisal <spisal@mrccitsolutions.com>
2022-04-14 21:13:25 +0530

    changes

--- src/app/shared/pipes/search-by-ctr.pipe.ts
+++ src/app/shared/pipes/search-by-ctr.pipe.ts
@@ -6,43 +6,51 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchByCTRPipe implements PipeTransform {
 
   transform(users: [], args: any): unknown {
-    if(args !== ""){
-      let usersAre = [];
-      let strArray = args.trim().toLowerCase().split(",");
-      users.filter((user : any)=>{
-        for(var str of strArray){
-          let strName = str.trim();
-          if(strName == ""){
-            break ;
-          }
-          if(user.candidateProfileKey){
-            if(user.candidateProfileKey.desiredRoles.toLowerCase().indexOf(strName) !== -1){
-              usersAre.push(user);
-              break ;
-            }else if(user.candidateProfileKey.desiredCompanies.toLowerCase().indexOf(strName) !== -1){
-              usersAre.push(user);
-              break ;
-            }else if(user.candidateProfileKey.introduceYouToo.toLowerCase().indexOf(strName) !== -1){
-              usersAre.push(user);
+    try{
+      if(args !== ""){
+        let usersAre = [];
+        let names = args.trim() ? args.trim() : "";
+        let strArray = names.toLowerCase().split(",");
+        users.filter((user : any)=>{
+          for(var str of strArray){
+            let strName = str.trim();
+            if(strName == ""){
               break ;
             }
-          }else if(user.candidateProfileKey == undefined){
-            if(user.desiredRoles.toLowerCase().indexOf(strName) !== -1){
-              usersAre.push(user);
-              break ;
-            }else if(user.desiredCompanies.toLowerCase().indexOf(strName) !== -1){
-              usersAre.push(user);
-              break ;
-            }else if(user.introduceYouToo.toLowerCase().indexOf(strName) !== -1){
-              usersAre.push(user);
-              break ;
+            if(user.candidateProfileKey ){
+              // if(user.candidateProfileKey.desiredRoles.toLowerCase().indexOf(strName) !== -1){
+              //   usersAre.push(user);
+              //   break ;
+              // }else if(user.candidateProfileKey.desiredCompanies.toLowerCase().indexOf(strName) !== -1){
+              //   usersAre.push(user);
+              //   break ;
+              // }else 
+              if((user.candidateProfileKey && user.candidateProfileKey.introduceYouToo && user.candidateProfileKey.introduceYouToo.toLowerCase().indexOf(strName) !== -1) || (user.fullName && user.fullName.toLowerCase().indexOf(strName) !== -1)){
+                usersAre.push(user);
+                break ;
+              }
+            }else if(!user.candidateProfileKey){
+              // if(user.desiredRoles && user.desiredRoles.toLowerCase().indexOf(strName) !== -1){
+              //   usersAre.push(user);
+              //   break ;
+              // }else if(user.desiredCompanies && user.desiredCompanies.toLowerCase().indexOf(strName) !== -1){
+              //   usersAre.push(user);
+              //   break ;
+              // }else 
+              if((user.introduceYouToo && user.introduceYouToo.toLowerCase().indexOf(strName) !== -1) || (user.candidate_id && user.candidate_id.fullName && user.candidate_id.fullName.toLowerCase().indexOf(strName) !== -1)){
+                usersAre.push(user);
+                break ;
+              }
             }
           }
-        }
-      });
-      return usersAre;
-    }else{
-      return users;
+        });
+        console.log("usersAre : ",usersAre);
+        return usersAre;
+      }else{
+        return users;
+      }
+    }catch(err){
+      console.log("err : ",err);
     }
   }
 
