813ca923cf57204cb12ca240df50015477260505
shankarpisal <spisal@mrccitsolutions.com>
2022-04-22 14:41:14 +0530

    changes

--- src/app/shared/shared-components/components/friends-connections/friends-connections.component.ts
+++ src/app/shared/shared-components/components/friends-connections/friends-connections.component.ts
@@ -45,6 +45,9 @@ export class FriendsConnectionsComponent extends AbstractSharedComponent impleme
   itemsIs = 0;
 
   thxFullObj;
+
+  introsToCompaniesCount : number = 0;
+  myNetworkCount : number = 0;
   
   constructor(
     protected _userService: UserService,
@@ -158,6 +161,12 @@ export class FriendsConnectionsComponent extends AbstractSharedComponent impleme
         this.requestedFriendAre = [friend, ...this.requestedFriendAre];
       }
     });
+    this.increseCount();
+  }
+
+  increseCount(){
+    this.introsToCompaniesCount = this.companiesUsresAre.length;
+    this.myNetworkCount = this.friendsConnections.length;
   }
 
   getUserWithRespectCompanies(friend){
@@ -229,6 +238,7 @@ export class FriendsConnectionsComponent extends AbstractSharedComponent impleme
 
   addFriendToFriendConnection(res){
     this.friendsConnections = [res, ...this.friendsConnections];
+    this.increseCount();
   }
 
   addRequestToRequestFriendConnection(res){
