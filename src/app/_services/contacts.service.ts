3cd6a740a1224b36cf37227007cdad4d973e8806
shankarpisal <spisal@mrccitsolutions.com>
2022-08-25 18:02:21 +0530

    changes

--- src/app/_services/contacts.service.ts
+++ src/app/_services/contacts.service.ts
@@ -11,20 +11,13 @@ export class ContactsService {
   }
   
   async getPermission(){
-    console.log("getPermission : ");
-    Materialize.toast("getPermission : ",1000);
     this.permissionStatus = await Contacts.getPermissions();
-    console.log("this.permissionStatus : ",this.permissionStatus);
-    Materialize.toast("status : "+this.permissionStatus, 2000);
   }
 
   async getContacts(){
-    console.log("getContacts : ");
-    Materialize.toast("getContacts : ",1000);
     Contacts.getContacts().then(result => {
       console.log(result);
       for(const contact of result.contacts) {
-        console.log("contact : ",contact);
         Materialize.toast("contact : "+contact, 2000);
       }
   });
