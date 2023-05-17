2dcc7eacf0416b3e8e2eee2ab00450e56ec92460
shankarpisal <spisal@mrccitsolutions.com>
2023-05-17 12:06:43 +0530

    changes

--- src/app/candidate/candidate-navbar/candidate-navbar.component.ts
+++ src/app/candidate/candidate-navbar/candidate-navbar.component.ts
@@ -31,10 +31,16 @@ export class CandidateNavbarComponent implements OnInit {
   ngOnInit() {
     this.getIAmAsARecruiterOrNot();
     this.userProfile = new Profile();
-    this.tabs1.push(
-      new Tab("/candidate/all-recruiters", "Recruiters Market Place", true)
-    );
+    // this.tabs1.push(
+    //   new Tab("/candidate/all-recruiters", "Recruiters Market Place", true)
+    // );
     this.tabs1.push(new Tab("/candidate/my-profile", "My Profile", false));
+    this.tabs1.push(new Tab2("/candidate/suggest-introduce", "Jobs", false, "fas fa-user"));
+    this.tabs1.push(new Tab2("/candidate/applied-jobs", "Applied jobs", false, "fas fa-user"));
+    this.tabs1.push(new Tab("/candidate/friends-connections", "HireSeat Connections", false));
+    this.tabs1.push(new Tab("/candidate/suggest-connected-friends", "Intros", false));
+
+
     this.tabs1.push(
       new Tab("/candidate/my-reviewed-profiles", "My Reviewed Profiles", false)
     );
@@ -42,18 +48,18 @@ export class CandidateNavbarComponent implements OnInit {
       new Tab("/candidate/interview-room", "Interview Room", false)
     );
     this.tabs1.push(
-      new Tab("/candidate/my-posted-profiles", "My Posted Profiles", false)
+      new Tab("/candidate/user-chat", "Messaging", false)
     );
-    this.tabs1.push(new Tab("/candidate/all-only-candidate-shared-profile", "HireSeat Network", false));
-    this.tabs1.push(new Tab("/candidate/friends-connections", "HireSeat Connections", false));
-    this.tabs1.push(new Tab("/candidate/suggest-connected-friends", "Suggest Connected friends", false));
 
-    this.tabs1.push(new Tab2("/candidate/suggest-introduce", "Jobs", false, "fas fa-user"));
-    this.tabs1.push(new Tab2("/candidate/applied-jobs", "Applied jobs", false, "fas fa-user"));
+    // this.tabs1.push(
+    //   new Tab("/candidate/my-posted-profiles", "My Posted Profiles", false)
+    // );
+    // this.tabs1.push(new Tab("/candidate/all-only-candidate-shared-profile", "HireSeat Network", false));
 
-    this.tabs1.push(
-      new Tab("/candidate/bidding-event-list", "Job Posting", false)
-    );
+
+    // this.tabs1.push(
+    //   new Tab("/candidate/bidding-event-list", "Job Posting", false)
+    // );
 
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -99,7 +105,7 @@ export class CandidateNavbarComponent implements OnInit {
     promises.push(this.userService.getUsersById().toPromise());
     Promise.all(promises).then(result => {
       if(result){
-        this.tabs1.splice(9, 0, new Tab2("/candidate/my-post-jobs", "My jobs List", false, "fas fa-user"));
+        this.tabs1.splice(1, 0, new Tab2("/candidate/my-post-jobs", "My jobs List", false, "fas fa-user"));
       }
     });
   }
