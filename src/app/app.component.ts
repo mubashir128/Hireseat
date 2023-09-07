8feb0f7f96224124f3f6bc568a8ce27d6934955b
rahim <rahim@sdsol.com>
2023-09-08 00:58:25 +0500

    background changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { App, AppState, URLOpenListenerEvent } from '@capacitor/app';
 import { BackgroundMode } from '@anuradev/capacitor-background-mode'
 import { Subscription } from "rxjs";
+import { KeepAwake } from '@capacitor-community/keep-awake';
 
 @Component({
   selector: "app-root",
@@ -31,31 +32,49 @@ export class AppComponent {
       });
     });
   }
-
+  keepAwake = async () => {
+    await KeepAwake.keepAwake();
+  };
   startChecks() {
     // Listen for app state changes
     App.addListener('appStateChange', async (state: AppState) => {
       if (state.isActive) {
-        // App is brought back to the foreground
-        BackgroundMode.moveToForeground();
-        BackgroundMode.wakeUp();
-        BackgroundMode.disable();
+        console.log('App resumed : ', state);
         this.initializeApp();
-        console.log('App resumed');
-        // Perform actions or resume tasks as needed.
       } else {
         // App is sent to the background
-        console.log('App paused');
-        BackgroundMode.enable();
-        // const res = await BackgroundMode.checkBatteryOptimizations();
-        // if (res.disabled) {
-        //    await BackgroundMode.requestDisableBatteryOptimizations();
-        // }
-        BackgroundMode.moveToBackground();
-        // Perform actions or save state before the app goes into the background.
+        console.log('App paused : ', state);
+        this.keepAwake();
       }
     });
 
   }
+  // startChecks() {
+  //   // Listen for app state changes
+  //   App.addListener('appStateChange', async (state: AppState) => {
+  //     if (state.isActive) {
+  //       // App is brought back to the foreground
+  //       BackgroundMode.moveToForeground();
+  //       BackgroundMode.wakeUp();
+  //       BackgroundMode.disable().then((res: any) => {
+  //         console.log("BackgroundMode disable : ", res)
+  //         this.initializeApp();
+  //       });
+  //       console.log('App resumed : ',state);
+  //       // Perform actions or resume tasks as needed.
+  //     } else {
+  //       // App is sent to the background
+  //       console.log('App paused : ',state);
+  //       BackgroundMode.enable();
+  //       // const res = await BackgroundMode.checkBatteryOptimizations();
+  //       // if (res.disabled) {
+  //       //    await BackgroundMode.requestDisableBatteryOptimizations();
+  //       // }
+  //       BackgroundMode.moveToBackground();
+  //       // Perform actions or save state before the app goes into the background.
+  //     }
+  //   });
+
+  // }
 
 }
