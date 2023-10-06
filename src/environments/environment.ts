b12e62d54a85d8777599d1622af372767cfe1c9d
shankarpisal <spisal@mrccitsolutions.com>
2023-10-06 12:18:21 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  baseUrl: "http://54.211.63.79:5000/",
-  socketUrl: "ws://54.211.63.79:5000/webSocket",
-  chatRedirectUrl : "http://54.211.63.79:4200/",
-  redirecUrl : "http://54.211.63.79:4200/",
+  baseUrl: "http://localhost:5000/",
+  socketUrl: "ws://localhost:5000/webSocket",
+  chatRedirectUrl : "http://localhost:4200/",
+  redirecUrl : "http://localhost:4200/",
   redirectAppleUrl : "https://bluerecruting.firebaseapp.com/__/auth/handler",
   serverId : "861355719085-slte0lj3ge7riviat156g898463d6mp1.apps.googleusercontent.com",
   appleUrl: "com.hireseat.app",
@@ -16,5 +16,5 @@ export const environment = {
     messagingSenderId: "861355719085",
     appId: "1:861355719085:web:d7d0b93cfe920c68944924"
   },
-  platformUrl : "54.211.63.79"
+  platformUrl : "localhost"
 };
