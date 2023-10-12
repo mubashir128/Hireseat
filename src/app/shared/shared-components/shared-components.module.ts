032f937c57a2886e8b803351f70dfc7aeec45afd
shankarpisal <spisal@mrccitsolutions.com>
2023-10-12 19:25:21 +0530

    changes for tabs mix in hireseat network

--- src/app/shared/shared-components/shared-components.module.ts
+++ src/app/shared/shared-components/shared-components.module.ts
@@ -151,6 +151,7 @@ import { IntrosToCompaniesComponent } from './components/intros-to-companies/int
 import { TimelineCommentsComponent } from './components/timeline-comments/timeline-comments.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { TopUsersComponent } from './components/top-users/top-users.component';
+import { ConnectionRequestsComponent } from './components/connection-requests/connection-requests.component';
 
 @NgModule({
   imports: [
@@ -325,7 +326,8 @@ import { TopUsersComponent } from './components/top-users/top-users.component';
     DialogSelectButttonsComponent,
     IntrosToCompaniesComponent,
     TimelineCommentsComponent,
-    TopUsersComponent
+    TopUsersComponent,
+    ConnectionRequestsComponent
   ],
   exports: [
     FooterComponent,
